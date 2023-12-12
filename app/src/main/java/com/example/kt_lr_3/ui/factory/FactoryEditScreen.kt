b3b23a91e6952e9bbc751ee172bb85e1c951ea5d
Slavik <slaw.z2014@gmail.com>
2023-12-12 11:45:51 +0200

    final

--- app/src/main/java/com/example/kt_lr_3/ui/factory/FactoryEditScreen.kt
+++ app/src/main/java/com/example/kt_lr_3/ui/factory/FactoryEditScreen.kt
@@ -1,6 +1,8 @@
 package com.example.kt_lr_3.ui.factory
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -51,6 +53,8 @@ fun FactoryEditScreen(
                 }
             },
             modifier = Modifier.padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+
         )
     }
 }
